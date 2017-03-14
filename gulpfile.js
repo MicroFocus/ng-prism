65f5a85d19dc194e80f8a951bb32c894ab070887
Joe Hawkins <jedwardhawkins@gmail.com>
2017-03-14 09:39:44 -0600

    Updated to version 0.6.1 of ng-gulp to address issues with karma configuration.

--- gulpfile.js
+++ gulpfile.js
@@ -23,6 +23,15 @@ ngGulp(gulp, {
             'node_modules/angular-material/angular-material.js',
             'node_modules/angular-material/angular-material.css',
             'node_modules/angular-ui-router/release/angular-ui-router.js'
+        ],
+        vendorTest: [
+            'node_modules/angular/angular.js',
+            'node_modules/angular-mocks/angular-mocks.js',
+            'node_modules/angular-animate/angular-animate.js',
+            'node_modules/angular-aria/angular-aria.js',
+            'node_modules/angular-material/angular-material.js',
+            'node_modules/angular-material/angular-material.css',
+            'node_modules/angular-ui-router/release/angular-ui-router.js'
         ]
     }
 });
