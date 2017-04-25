9124dfc3c2f952aaec4f18b38a193bd82ecc9208
Joseph White <joseph.white@microfocus.com>
2017-04-25 16:53:24 -0600

    Compile elements using  inside the directive and stop using global scope

--- src/directives/toggle-repeat-code.directive.ts
+++ src/directives/toggle-repeat-code.directive.ts
@@ -1,9 +1,10 @@
-import {element, IAttributes, IAugmentedJQuery, IScope} from 'angular';
+import {element, IAttributes, IAugmentedJQuery, ICompileService, IScope} from 'angular';
 
 export class ToggleRepeatCodeController {
     visible: boolean;
 
     static $inject = ['$scope'];
+
     constructor(private $scope: IScope) {
         this.visible = false;
     }
@@ -17,23 +18,35 @@ export class ToggleRepeatCodeController {
     }
 }
 
-export default ['RepeatAsCodeService',
-    (RepeatAsCodeService) => {
-        return {
-            controller: ToggleRepeatCodeController,
-            controllerAs: '$ctrl',
-            priority: 1000,
-            restrict: 'A',
-            compile: (tElement: IAugmentedJQuery, attr: IAttributes) => {
-                tElement.removeAttr('toggle-repeat-code');
-                let code = RepeatAsCodeService(tElement, attr.toggleRepeatCode);
-                code.attr('ng-show', '$ctrl.visible');
-                let toggleShow = element(`<toggle-show on-hide="$ctrl.hide()" on-show="$ctrl.show()"></toggle-show>`);
-                let parent = element(`<div></div>`);
-                parent.append(toggleShow);
-                parent.append(code);
-                tElement.after(parent);
-            }
-        };
-    }
-];
+export default ['RepeatAsCodeService', '$compile', ToggleRepeatDirective];
+
+export function ToggleRepeatDirective(RepeatAsCodeService, $compile: ICompileService) {
+    return {
+        controller: ToggleRepeatCodeController,
+        controllerAs: '$ctrl',
+        priority: 1000,
+        restrict: 'A',
+        scope: {
+            toggleRepeatCode: '@',
+        },
+        compile: (tElement: IAugmentedJQuery, tAttrs: IAttributes) => {
+            // Highlight code
+            tElement.removeAttr('toggle-repeat-code');
+            let code = RepeatAsCodeService(tElement, tAttrs.toggleRepeatCode);
+
+            // Create an element containing the code and a toggle-show directive
+            code.attr('ng-show', '$ctrl.visible');
+            let toggleShow = element(`<toggle-show on-hide="$ctrl.hide()" on-show="$ctrl.show()"></toggle-show>`);
+            let parent = element(`<div></div>`);
+            parent.append(toggleShow);
+            parent.append(code);
+            let linkFn = $compile(parent, null, 100);
+
+            // Bind the directive to this controller
+            return (scope: IScope, iElement: IAugmentedJQuery, iAttrs: IAttributes) => {
+                let content = linkFn(scope);
+                iElement.after(content);
+            };
+        }
+    };
+}
