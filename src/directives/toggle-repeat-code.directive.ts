2ff83054d6bea31ba52ad024449dd530b48c6bad
Joseph White <joseph.white@microfocus.com>
2017-05-01 17:03:42 -0600

    Make css transitions work

--- src/directives/toggle-repeat-code.directive.ts
+++ src/directives/toggle-repeat-code.directive.ts
@@ -39,13 +39,14 @@ export function ToggleRepeatDirective(RepeatAsCodeService: IRepeatAsCodeService,
             // Highlight code
             tElement.removeAttr('toggle-repeat-code');
             let code = RepeatAsCodeService(tElement, tAttrs.toggleRepeatCode);
+            let codeElement = element(`<div class="prism-toggleable-code"></div>`).append(code);
 
             // Create an element containing the code and a toggle-show directive
-            code.attr('ng-class', `[{'prism-hidden': $ctrl.hidden}]`);
+            codeElement.attr('ng-class', `[{'prism-hidden': $ctrl.hidden}]`);
             let toggleShow = element(`<toggle-show on-hide="$ctrl.hide()" on-show="$ctrl.show()"></toggle-show>`);
             let parent = element(`<div></div>`);
             parent.append(toggleShow);
-            parent.append(code);
+            parent.append(codeElement);
             let linkFn = $compile(parent, null, 100);
 
             // Bind the directive to this controller
