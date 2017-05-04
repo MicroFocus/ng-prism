5ce563d623c9f717904c179bea060abc3139bb09
Joseph White <joseph.white@microfocus.com>
2017-05-04 11:49:17 -0600

    Remove unneeded  on toggle-repeat-code directive

--- src/directives/toggle-repeat-code.directive.ts
+++ src/directives/toggle-repeat-code.directive.ts
@@ -6,24 +6,17 @@
 import {element, IAttributes, IAugmentedJQuery, ICompileService, IScope} from 'angular';
 import {IRepeatAsCodeService} from '../services/repeat-as-code.service';
 
-export default ['RepeatAsCodeService', '$compile', ToggleRepeatDirective];
+export default ['RepeatAsCodeService', ToggleRepeatDirective];
 
-export function ToggleRepeatDirective(RepeatAsCodeService: IRepeatAsCodeService, $compile: ICompileService) {
+export function ToggleRepeatDirective(RepeatAsCodeService: IRepeatAsCodeService) {
     return {
         priority: 1000,
         restrict: 'A',
         compile: (tElement: IAugmentedJQuery, tAttrs: IAttributes) => {
-            // Create toggleable code block
             tElement.removeAttr('toggle-repeat-code');
             let code = RepeatAsCodeService(tElement, tAttrs.toggleRepeatCode);
             let toggleableCode = element(`<toggleable-code></toggleable-code>`).append(code);
-            let linkFn = $compile(toggleableCode);
-
-            // Compile the code block
-            return (scope: IScope, iElement: IAugmentedJQuery) => {
-                let content = linkFn(scope);
-                iElement.after(content);
-            };
+            tElement.after(toggleableCode);
         }
     };
 }
